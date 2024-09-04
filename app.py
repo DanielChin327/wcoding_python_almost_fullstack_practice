51368b0d750f8188321263c76cfeaadad8ff336a
Daniel Chin <DanChin327@gmail.com>
2024-09-04 11:27:56 +0900

    git init

---
+++ app.py
@@ -0,0 +1,5 @@
+import socketserver # allow python to listen to ports, and get rests from the web/elsewhere
+import http.server # allows python to serve HTTP requests
+
+
+import urllib.parse #manipulate URLS and query Strings
